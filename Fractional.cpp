045196d6870cea6db024a712ae52615cd76b6073
Dinu Ion George <32941751+DinuGeorge0019@users.noreply.github.com>
2021-03-09 23:23:09 +0200

    Add files via upload

---
+++ Fractional.cpp
@@ -0,0 +1,292 @@
+#include "fractional.h"
+
+
+Fractional::Fractional(long long numerator, long long dominator)
+{
+	this->numerator = numerator;
+	this->denominator = dominator;
+}
+
+Fractional::Fractional(long long number)
+{
+	this->numerator = number;
+	this->denominator = 1;
+}
+
+Fractional::Fractional()
+{
+	this->numerator = 0;
+	this->denominator = 1;
+}
+
+Fractional& Fractional::operator = (const Fractional& other)
+{
+	if (this == &other)
+		return *this;
+
+	this->numerator = other.numerator;
+	this->denominator = other.denominator;
+	return *this;
+}
+
+Fractional operator* (const Fractional& f1, const Fractional& f2)
+{
+	Fractional result;
+	result.numerator = f1.numerator * f2.numerator;
+	result.denominator = f1.denominator * f2.denominator;
+	!result;
+	return result;
+}
+
+
+Fractional operator/ (const Fractional& f1, const Fractional& f2)
+{
+	Fractional result;
+	result.numerator = f1.numerator * f2.denominator;
+	result.denominator = f1.denominator * f2.numerator;
+	!result;
+	return result;
+}
+
+long long gcd(long long a, long long b)
+{
+	if (b == 0) return a;
+	a %= b;
+	return gcd(b, a);
+}
+
+Fractional& Fractional::operator! ()
+{
+	long long GCD = gcd(numerator, denominator);
+
+	numerator = numerator / GCD;
+	denominator = denominator / GCD;
+
+	if (denominator < 0)
+	{
+		denominator = (-1) * denominator;
+		numerator = (-1) * numerator;
+	}
+
+	return *this;
+}
+
+Fractional operator + (const Fractional& f1, const Fractional& f2)
+{
+	Fractional result;
+
+	if (f1.denominator == f2.denominator)
+	{
+		result.numerator = f1.numerator + f2.numerator;
+		result.denominator = f1.denominator;
+		!result;
+		return result;
+	}
+	else
+	{
+		Fractional  f1_copy = f1;
+		Fractional  f2_copy = f2;
+		!f1_copy;
+		!f2_copy;
+		if (f1_copy.denominator == f2_copy.denominator)
+		{
+			return f1_copy + f2_copy;
+		}
+		result.numerator = f1.numerator * f2.denominator + f1.denominator * f2.numerator;
+		result.denominator = f1.denominator * f2.denominator;
+	}
+
+	!result;
+	return result;
+
+}
+
+Fractional operator - (const Fractional& f1, const Fractional& f2)
+{
+
+	Fractional result;
+
+	if (f1.denominator == f2.denominator)
+	{
+		result.numerator = f1.numerator - f2.numerator;
+		result.denominator = f1.denominator;
+		!result;
+		return result;
+	}
+	else
+	{
+		Fractional  f1_copy = f1;
+		Fractional  f2_copy = f2;
+		!f1_copy;
+		!f2_copy;
+		if (f1_copy.denominator == f2_copy.denominator)
+		{
+			return f1_copy - f2_copy;
+		}
+		result.numerator = f1.numerator * f2.denominator - f1.denominator * f2.numerator;
+		result.denominator = f1.denominator * f2.denominator;
+	}
+
+	!result;
+	return result;
+
+}
+
+Fractional Fractional::operator ^ (const int& power)
+{
+	if (power == 0)
+	{
+		return { 1, 1 };
+	}
+
+	if (power <= -1)
+	{
+		Fractional inversed(1);
+		inversed = 1 / (*this);
+		return inversed ^ (-power);
+	}
+
+	Fractional result = *this;
+	for (int i = 1; i < power; i++)
+	{
+		result = result * (*this);
+	}
+
+	return result;
+
+}
+
+ostream& operator<<(ostream& os, const Fractional& c)
+{
+
+	if (c.numerator == 0)
+	{
+		os << "0";
+		return os;
+	}
+
+	if (c.denominator == 1)
+	{
+		os << c.numerator;
+		return os;
+	}
+
+	if (c.denominator == c.numerator)
+	{
+		os << "1";
+		return os;
+	}
+
+	os << c.numerator << "/" << c.denominator;
+
+	return os;
+
+}
+
+istream& operator>>(istream& is, Fractional& c)
+{
+	is >> c.numerator;
+	is >> c.denominator;
+	return is;
+
+}
+
+bool operator == (const  Fractional& f1, const  Fractional& f2)
+{
+	Fractional copy_f1 = f1;
+	Fractional copy_f2 = f2;
+	!copy_f1;
+	!copy_f2;
+
+	if (copy_f1.numerator == copy_f2.numerator && copy_f1.denominator == copy_f2.denominator)
+		return true;
+
+	return false;
+}
+
+bool operator < (const  Fractional& f1, const  Fractional& f2)
+{
+	Fractional copy_f1 = f1;
+	Fractional copy_f2 = f2;
+	!copy_f1;
+	!copy_f2;
+
+	if (copy_f1.denominator == copy_f2.denominator)
+	{
+		return copy_f1.numerator < copy_f2.numerator;
+	}
+	else
+	{
+		copy_f1.numerator = copy_f1.numerator * copy_f2.denominator;
+		copy_f2.numerator = copy_f2.numerator * copy_f1.denominator;
+
+		return copy_f1.numerator < copy_f2.numerator;
+	}
+}
+
+
+
+bool operator > (const  Fractional& f1, const  Fractional& f2)
+{
+	Fractional copy_f1 = f1;
+	Fractional copy_f2 = f2;
+	!copy_f1;
+	!copy_f2;
+
+	if (copy_f1.denominator == copy_f2.denominator)
+	{
+		return copy_f1.numerator > copy_f2.numerator;
+	}
+	else
+	{
+		copy_f1.numerator = copy_f1.numerator * copy_f2.denominator;
+		copy_f2.numerator = copy_f2.numerator * copy_f1.denominator;
+
+		return copy_f1.numerator > copy_f2.numerator;
+	}
+}
+
+
+bool operator <= (const  Fractional& f1, const  Fractional& f2)
+{
+
+	if (f1 == f2 || f1 < f2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+bool operator >= (const  Fractional& f1, const  Fractional& f2)
+{
+	if (f1 == f2 || f1 > f2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+Fractional Fractional::get_abs()
+{
+	Fractional f = *this;
+
+	if (f.numerator < 0)
+	{
+		f.numerator = f.numerator * (-1);
+	}
+
+	if (f.denominator < 0)
+	{
+		f.denominator = f.denominator * (-1);
+	}
+
+	return f;
+}
---
+++ Gauss_pt.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "Fractional.h"
+using namespace std;
+
+
+
+void Gauss_pt(vector<vector<Fractional>>& matrix, int n, fstream& out_file)
+{
+
+	const Fractional E(1, pow(10, 9));
+
+	int npc = 0;
+
+	vector<pair<int, int>> c;
+
+
+	for (auto i : matrix)
+	{
+		for (auto j : i)
+		{
+			cout << j << " ";
+		}
+		cout << endl;
+	}
+
+	for (int k = 1; k <= n - 1; k++)
+	{
+		Fractional piv = matrix[k][k].get_abs();
+		int lin = k;
+		int col = k;
+
+		for (int i = k; i <= n; i++)
+		{
+			for (int j = k; j <= n; j++)
+			{
+				if (piv < matrix[i][j].get_abs())
+				{
+					piv = matrix[i][j].get_abs();
+					lin = i;
+					col = j;
+				}
+			}
+		}
+
+		if (piv <= E)
+		{
+			out_file << "Sistemul nu are solutie unica" << endl;
+			out_file << endl;
+			return;
+		}
+
+		if (lin != k)
+		{
+			for (int j = k; j <= n + 1; j++)
+			{
+				swap(matrix[k][j], matrix[lin][j]);
+			}
+		}
+
+		if (col != k)
+		{
+			npc++;
+			c.push_back({ k, col });
+			for (int i = 1; i <= n; i++)
+			{
+				swap(matrix[i][k], matrix[i][col]);
+			}
+
+		}
+
+		for (auto i : matrix)
+		{
+			for (auto j : i)
+			{
+				cout << j << " ";
+			}
+			cout << endl;
+		}
+
+		for (int i = k + 1; i <= n; i++)
+		{
+			matrix[i][k] = matrix[i][k] / matrix[k][k];
+			for (int j = k + 1; j <= n + 1; j++)
+			{
+				matrix[i][j] = matrix[i][j] - matrix[i][k] * matrix[k][j];
+			}
+		}
+
+
+		for (auto i : matrix)
+		{
+			for (auto j : i)
+			{
+				cout<<j<<" ";
+			}
+			cout << endl;
+		}
+
+
+
+	}
+
+	if (matrix[n][n].get_abs() <= E)
+	{
+		out_file << "Sistemul nu are solutie unica" << endl;
+		out_file << endl;
+		return;
+	}
+
+	matrix[n][n + 1] = matrix[n][n + 1] / matrix[n][n];
+
+	for (int i = n - 1; i >= 1; i--)
+	{
+		Fractional s;
+		for (int j = i + 1; j <= n; j++)
+		{
+			s = s + matrix[i][j] * matrix[j][n + 1];
+		}
+		matrix[i][n + 1] = (matrix[i][n + 1] - s) / matrix[i][i];
+	}
+
+	if (npc != 0)
+	{
+		for (int i = npc-1; i >= 0; i--)
+		{
+			swap(matrix[c[i].first][n+1], matrix[c[i].second][n+1]);
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		out_file << "X[" << i << "] = " << matrix[i][n + 1] << endl;
+	}
+	out_file << endl;
+
+}
+
+
+int main()
+{
+
+	fstream in_file, out_file;
+	in_file.open("input.txt", ios::in);
+	out_file.open("output_Gauss_pt.txt", ios::out | ios::trunc);
+
+
+	int cases = 1;
+
+	while (cases <= 3)
+	{
+		// reading the input from file 
+
+		int n;
+		in_file >> n;
+		vector<vector<Fractional>> matrix(n + 1, vector<Fractional>(n + 2));
+
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n + 1; j++)
+			{
+				in_file >> matrix[i][j];
+			}
+		}
+
+		out_file << "Sistem: " << cases << endl;
+
+		// solving the given system
+
+		Gauss_pt(matrix, n, out_file);
+
+		cases++;
+	}
+
+	in_file.close();
+	out_file.close();
+
+	return 0;
+}
